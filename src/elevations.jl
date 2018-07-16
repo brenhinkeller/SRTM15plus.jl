9b9a1fb4894105127c44f7162993ffaf674a2f8b
C. Brenhin Keller <cbkeller@princeton.edu>
2018-07-15 23:24:56 -0700

    Try to fix data path

--- src/elevations.jl
+++ src/elevations.jl
@@ -1,14 +1,18 @@
+# Establish data path
+datapath = joinpath(Pkg.dir("SRTM15plus"),"data")
+filepath = joinpath(datapath, "SRTM15plus.nc")
+
 # Download NetCDF file from Google Cloud if necessary
-if ~isfile("data/SRTM15plus.nc") || ~stat("data/SRTM15plus.nc").size==2783012160
+if ~isfile(filepath) || ~stat(filepath).size==2783012160
     print("Downloading SRTM15plus.nc from google cloud storage\n")
-    mkpath("data") # Ensure directory exists
-    download("https://storage.googleapis.com/statgeochem/SRTM15plus.nc","data/SRTM15plus.nc")
+    mkpath(datapath) # Ensure directory exists
+    download("https://storage.googleapis.com/statgeochem/SRTM15plus.nc", filepath)
 end
 
 function loadSRTM15plus()
     # Read NetCDF file
     print("Reading from NetCDF:\n")
-    @time elevation = ncread("data/SRTM15plus.nc", "z")
+    @time elevation = ncread(filepath, "z")
     return elevation
 end
 export loadSRTM15plus
